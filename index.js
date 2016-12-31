ba5a19e7c18b73fee2da698cd7de6a120dd4a707
jonschlinkert <jon.schlinkert@sellside.com>
2016-12-31 00:41:48 -0500

    use has-values

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
 var isPrimitive = require('is-primitive');
 var omitEmpty = require('omit-empty');
-var has = require('has-value');
+var has = require('has-values');
 
 module.exports = function(answer) {
   return isPrimitive(answer) ? has(answer) : has(omitEmpty(answer));
